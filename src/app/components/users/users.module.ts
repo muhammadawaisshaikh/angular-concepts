0b52544c2553837270cfbc9c30f03c15dcafe6a1
Muhammad Awais <awaisshaikh94@gmail.com>
2022-05-30 02:18:44 +0500

    added a view-user with queryParamed route

--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -9,12 +9,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { UsersListingComponent } from './users-listing/users-listing.component';
 import { AddEditUserComponent } from './add-edit-user/add-edit-user.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 @NgModule({
   declarations: [
     UsersComponent,
     UsersListingComponent,
-    AddEditUserComponent
+    AddEditUserComponent,
+    ViewUserComponent
   ],
   imports: [
     CommonModule,
